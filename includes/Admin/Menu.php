0f723b2908223199f3ccbd49ad313baaf381e34b
Osman Haider <osmanhaider159@gmail.com>
2024-05-03 01:15:49 +0600

    init

---
+++ includes/Admin/Menu.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Order\Backup\Admin;
+
+/**
+ * The menu handler class
+ */
+class Menu {
+
+    public function __construct() {
+        add_action('admin_menu', [$this, 'admin_menu']);
+    }
+
+    public function admin_menu()
+    {
+        add_menu_page(
+            __('Backup Order', 'backup-order'),
+            __('Backup Order', 'backup-order'),
+            'manage_options',
+            'backup-order',
+            [$this, 'plugin_page']
+        );
+    }
+
+    /**
+     * Callback function to display the plugin page.
+     */
+    public function plugin_page() {
+        echo "hello";
+    }
+}
